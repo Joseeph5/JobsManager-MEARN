22849c0a962c2e2cec3adbe2459a218d11f37f26
joseeph5 <youssef.beenamor@gmail.com>
2022-01-14 15:39:18 +0100

    Dashboard Structure Setup

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/Navbar';
+import { AiFillHome } from 'react-icons/ai';
+
+function NavBar() {
+  return (
+    <Wrapper>
+      <h4>navbar</h4>
+      <AiFillHome />
+    </Wrapper>
+  );
+}
+
+export default NavBar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, Navigate, Outlet } from 'react-router-dom';
-import Wrapper from '../assets/wrappers/SharedLayout';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { SmallSideBar, BigSideBar, Navbar, NavBar } from '../components';
 
 function Dashboard() {
   const { user } = useSelector((state) => state);
@@ -11,11 +12,16 @@ function Dashboard() {
   }
   return (
     <Wrapper>
-      <nav>
-        <Link to='all-jobs'>all jobs</Link>
-        <Link to='add-job'>add jobs</Link>
-      </nav>
-      <Outlet />
+      <div className='dashboard'>
+        <SmallSideBar />
+        <BigSideBar />
+        <div>
+          <NavBar />
+          <div className='dashboard-page'>
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 }
